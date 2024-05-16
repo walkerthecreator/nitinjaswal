08e051d5d4a902ce0c9709f2ae4ad1c2e992071e
walkerthecreator <nitinjaswal2616@gmail.com>
2024-05-16 23:31:36 +0530

    Added Web Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Nav";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Nav></Nav>
         {children}
+        <Analytics/>
         </body>
     </html>
   );
