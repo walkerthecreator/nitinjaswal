ed95e6c6fb6657da6583c45006f7f5d7d7f17f57
walkerthecreator <nitinjaswal2616@gmail.com>
2025-02-11 23:54:55 +0530

    revamped experince

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,9 @@ import { motion } from "framer-motion";
 import Button from "@/components/Button"
 import Link from "next/link";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Sparkle } from "lucide-react";
+import { MapPin, Sparkle } from "lucide-react";
 import { Instrument_Serif } from "next/font/google";
+import WorkExperienceCard from "@/components/WorkExperienceCard";
 
 const instrumentSerif = Instrument_Serif({ 
   weight: '400',
@@ -65,118 +66,95 @@ const icons = [
   }
 ];
 
+const workExperiences = [
+  {
+    title: "Full Stack Engineer",
+    date: "September, 2024 - Present",
+    isPresent: true,
+    points: [
+      "Developing a WLS to Offset carbon emissions with seamless integration.",
+      "Enabled real-time updates via configurable webhooks and callback mechanisms.",
+      "Architect solution with rate-limiting, caching, and auto-scaling mechanisms."
+    ]
+  },
+  {
+    title: "Full Stack Engineer",
+    date: "June, 2023 - June, 2024",
+    points: [
+      'Built an <span class="highlight">Image Annonation</span> tool for creating Annonations of Tractors for <span class="highlight">New Holland</span>.',
+      "Utilized NodeJs, MySQL and FabricJs as the core tech stack.",
+      "Assisted in the building and integrated a real-time monitoring system using Node.Js and WebSockets."
+    ]
+  },
+  {
+    title: "Frontend Developer Intern",
+    date: "January, 2023 - June 2023",
+    points: [
+      "Revamped UI and Improved UX for Various Client Web Apps",
+      "Contributed to web apps involving JavaScript, jQuery, CSS,HTML and MySQL.",
+      "Implemented backend functionalities, gaining hands-on experience in Node.js along with Express.js."
+    ]
+  }
+];
+
 export default function Home() {
 
   return (
     <>
     <div className="min-w-screen min-h-screen">
       <ScrollArea className="h-[calc(100vh-3.5rem)]">
+        <section className="flex flex-col justify-evenly -translate-y-10 items-center min-h-screen w-11/12  md:w-3/5 mx-auto">
+          <motion.h1 
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.4, ease: "easeOut" }}
+            className="w-full max-w-lg text-lg"
+          >
+            Hi, I&apos;m Nitin Jaswal, a Fullstack Engineer who&apos;s passionate about building products and experiences
+          </motion.h1>
+        </section>
 
-      <section className="flex flex-col justify-evenly -translate-y-10 items-center min-h-screen w-11/12  md:w-3/5 mx-auto">
-        
-        <motion.h1 
-          initial={{ y: 10, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.4, ease: "easeOut" }}
-          className="w-full max-w-lg text-lg"
-        >
-          Hi, I&apos;m Nitin Jaswal, a Fullstack Engineer who&apos;s passionate about building products and experiences
-        </motion.h1>
-
-      </section>
-      <section className="min-w-screen">
-        <div className="min-h-screen max-w-3xl mx-auto ">
-
-            <div className="rounded-lg bg-neutral-700/40 border border-neutral-600 w-full p-4">
-              <h1 className="">Experience</h1>
-              <div className="border-s ms-3">
-                <div className="mt-4 ms-4 relative">
-                  <div className="bg-green-300 rounded-full -left-[22px] top-2 p-[6px] absolute"></div>
-                  <h2 className="text-xl font-medium ">
-                    Full Stack Engineer
-                  </h2>
-                  <div className="">
-                    <h2 className="my-1 text-zinc-400 text-sm">
-                      September, 2024 - Present{" "}
-                    </h2>
-                    <ul className="list-disc list-inside mt-2">
-                      <li>Develop an WLS to calculate and offset user emissions with seamless integration ready to
-                      integrate with various platforms.</li>
-                      <li>Enable real-time updates via configurable webhooks and callback mechanisms.</li>
-                      <li>Architect the gateway with rate-limiting, caching, and auto-scaling
-                      mechanisms to ensure uptime and performance.</li>
-                    </ul>
-                  </div>
+        <section className="min-h-screen min-w-screen flex items-center justify-center">
+          <div className="max-w-3xl mx-auto ">
+              <div className=" w-full p-4">
+                <h1 className={`${instrumentSerif.className} text-3xl mb-10 text-center`}>Work</h1>
+                <div className="border-s border-neutral-700 ms-3 space-y-10">
+                { 
+                  workExperiences.map(work => (<WorkExperienceCard experience={work} /> ))
+                } 
                 </div>
-                <div className="mt-4 ms-4 relative">
-                  <div className="bg-zinc-400 rounded-full -left-[22px] top-2 p-[6px] absolute"></div>
-                  <h2 className="text-xl font-medium">
-                    Full Stack Engineer
-                  </h2>
-                  <div className="">
-                    <h2 className="my-1  text-sm">
-                      June, 2023 - June, 2024
-                    </h2>
-                    <ul className="list-disc list-inside mt-2">
-                      <li>Built an <span className="highlight">Image Annonation</span> tool for creating Annonations of Tractors for <span className="highlight">New Holland</span>.</li>
-                      <li>Utilized NodeJs, MySQL and FabricJs as the core tech stack.</li>
-                      <li>Assisted in the building and integrated a real-time monitoring system using Node.Js and WebSockets.</li>
-                    </ul>
-                  </div>
+            </div>
+          </div>
+        </section>
+
+
+        <section className="max-w-xl mx-auto min-h-screen flex flex-col gap-10 items-center justify-center">
+          <h1 className={`text-3xl ${instrumentSerif.className}`}>Tech Cloud</h1>
+          <div className="grid grid-cols-4 gap-8">
+            {icons.map((item , index) => (
+              <div key={index} className="flex flex-col items-center gap-2 group">
+                <img src={item.icon} alt={item.title} className="h-12 w-12 saturate-0 invert" />
+                <span className="text-xs opacity-0 group-hover:opacity-100 transition-opacity">{item.title}</span>
+              </div>
+            ))}
+
+          </div>
+        </section>
+
+        <section className="max-w-4xl mx-auto min-h-screen flex flex-col gap-10 items-center justify-center">
+            <div className="w-full h-full relative grid grid-cols-2 rounded-xl overflow-hidden">
+                <div className="bg-neutral-800 h-full p-6 flex flex-col items-center justify-center gap-4">
+                    <h2 className={`${instrumentSerif.className} font-bold text-4xl`}>Let's build your Next Big Idea</h2>
+                    <button className="w-full bg-white text-primary p-3 rounded-xl flex items-center mt-4 justify-center gap-2 ring ring-neutral-600">
+                    <Sparkle className="h-4 w-4" />Get in Touch</button>
+                    <div className="flex items-center gap-1">
+                      <div className="bg-green-500 h-2 w-2 rounded-xl "/>
+                      <span className="text-xs">Open for New Oppurtuinities</span>
+                    </div>
                 </div>
-
-                <div className="mt-6 ms-4 relative">
-                  <div className="bg-zinc-400 rounded-full -left-[22px] top-2 p-[6px] absolute"></div>
-                  <h2 className="text-xl font-medium">
-                    Frontend Developer Intern
-                  </h2>
-                  <div className="">
-                    <h2 className="my-1  text-sm">
-                      January, 2023 - June 2023
-                    </h2>
-                    <ul className=" list-disc list-inside mt-2">
-                      <li>Revamped UI and Improved UX for Various Client Web Apps </li>
-                      <li>Contributed to web apps  involving JavaScript, jQuery, CSS,HTML and MySQL.</li>
-                      <li>Implemented backend functionalities, gaining hands-on experience in Node.js
-                        along with Express.js.</li>
-                    </ul>
-                  </div>
-                {/* </div> */}
-              </div>
+                <img src="/images/me.jpg" alt="landscape" className="h-[500px] w-full object-cover object-top" />
             </div>
-            
-          </div>
-        </div>
-      </section>
-
-
-      <section className="max-w-xl mx-auto min-h-screen flex flex-col gap-10 items-center justify-center">
-        <h1 className="text-xl font-semibold">Tech Cloud</h1>
-        <div className="grid grid-cols-4 gap-8">
-          {icons.map((item , index) => (
-            <div key={index} className="flex flex-col items-center gap-2 group">
-              <img src={item.icon} alt={item.title} className="h-12 w-12 saturate-0 invert" />
-              <span className="text-xs opacity-0 group-hover:opacity-100 transition-opacity">{item.title}</span>
-            </div>
-          ))}
-
-        </div>
-      </section>
-
-      <section className="max-w-4xl mx-auto min-h-screen flex flex-col gap-10 items-center justify-center">
-          <div className="w-full h-full relative grid grid-cols-2 rounded-xl overflow-hidden">
-              <div className="bg-neutral-800 h-full p-6 flex flex-col items-center justify-center gap-4">
-                  <h2 className={`${instrumentSerif.className} font-bold text-4xl`}>Let's build your Next Big Idea</h2>
-                  <button className="w-full bg-primary p-3 rounded-xl flex items-center mt-4 justify-center gap-2 ring ring-neutral-700">
-                  <Sparkle className="h-4 w-4" />Get in Touch</button>
-                  <div className="flex items-center gap-1">
-                    <div className="bg-green-500 h-2 w-2 rounded-xl "/>
-                    <span className="text-xs">Open for New Oppurtuinities</span>
-                  </div>
-              </div>
-              <img src="/images/me.jpg" alt="landscape" className="h-[500px] w-full object-cover object-top" />
-          </div>
-      </section>
+        </section>
       </ScrollArea>
       </div>
     </>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 export default function Nav() {
 
   return (
-    <nav className="z-10 w-full fixed shadow-lg bottom-0 border-t bg-neutral-800 text-white border-neutral-600 mt-4 p-2 py-3 mx-auto flex justify-center gap-3 ">
+    <nav className="z-10 w-full fixed shadow-lg bottom-0 border-t bg-neutral-800 text-white border-neutral-600 mt-4 p-2  mx-auto flex justify-center gap-3 ">
     
       <div className="flex justify-between w-full max-w-2xl items-center">
       <div className="flex items-center gap-2">
-        <div className="bg-gradient-to-br font-medium from-green-400 to-lime-400 h-8 w-8 rounded-full text-lg" />
+        <div className="bg-gradient-to-br font-medium from-green-400 to-lime-400 h-7 w-7 rounded-full text-lg" />
         <Link href="/">Nitin Jaswal</Link>
       </div>
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-4 text-sm">
     <Link href="/projects" className="hover:-translate-y-1 transition-transform">Projects</Link>
     <Link href="/about" className="hover:-translate-y-1 transition-transform">About</Link>
       </div>
