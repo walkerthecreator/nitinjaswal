c6e703057a92021eb85d9c579f0aa51d25ac8bfc
walkerthecreator <nitinjaswal2616@gmail.com>
2025-02-12 23:52:07 +0530

    fix:drawer

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,9 +2,9 @@
 import Project from "@/components/Project";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 import { instrumentSerif } from "@/lib/font";
-import { Phone, PhoneMissed } from "lucide-react";
+import { Phone, PhoneMissed, X } from "lucide-react";
 
 const projectsData = [
   {
@@ -53,8 +53,8 @@ function Projects() {
   const [ drawer , setDrawer ] = useState(false)
 
   return (
-    <div className={"relative max-w-7xl mx-auto " + (drawer ? "max-h-screen overflow-hidden px-10"  : "" )}>
-    <div className={"bg-primary mx-auto md:w-4/5 md:pb-20 " + (drawer ? "opacity-80" : "opacity-100") }>
+    <div className={"relative max-w-7xl mx-auto flex items-center justify-center " + (drawer && "max-h-screen overflow-hidden px-10"  )}>
+    <div className={"bg-primary mx-auto md:w-4/5 md:pb-20 " + (drawer ? "opacity-20" : "opacity-100") }>
 {
 
   show &&
@@ -110,38 +110,41 @@ function Projects() {
         }
 
       </div>
-      </div>
+    </div>
 
       {
         drawer 
         && 
-        <motion.div
-        initial={{ y : 400 }}
-        animate={{ y : 0 }} 
-        exit={{ y : -400 }}
-        className="h-full bg-white w-full absolute rounded-t-3xl p-2 shadow-lg shadow-black top-10">
-
-            <button onClick={()=>{ setDrawer(false) }} className="bg-stone-100 p-3 rounded-full float-end">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
-            </button>
-
-          <h1 className="text-4xl font-medium py-4 text-center mt-10">Get in Touch </h1>
-
-            <div className="flex flex-col md:w-3/5 mx-auto mt-10 gap-20">
-              <div className="md:w-3/5 mx-auto flex flex-col">
-                <input className="p-2 px-3 bg-zinc-50 rounded-md border border-zinc-200" type="text" placeholder="Enter Name" name="name"/>
-                <input className="mt-2 p-2 px-3 bg-zinc-50 rounded-md border border-zinc-200" type="email" placeholder="Enter Email" name="email" />
-
-                <p className="text-zinc-400 my-2 text-sm text-center">We&apos;ll Get back to you with-in 24 hours.</p>
-              </div>
-
-              <button className="bg-zinc-700 w-3/5 mx-auto p-3 shadow-inner shadow-zinc-400  text-zinc-200 font-semibold rounded-lg">Book Free call</button>
-            </div>
-        </motion.div>
+        <Drawer setDrawer={setDrawer} />
       }
-      </ div>
+    </ div>
 
   );
 }
 
 export default Projects;
+
+export function Drawer({ setDrawer } : { setDrawer : React.Dispatch<SetStateAction<boolean>>  }){
+  return <motion.div
+  initial={{ y : 400 }}
+  animate={{ y : 0 }} 
+  exit={{ y : -400 }}
+  className="h-full bg-neutral-800 border border-neutral-600 w-full max-w-4xl mx-auto absolute rounded-t-3xl p-2 shadow-lg shadow-black top-10">
+
+      <button onClick={()=>{ setDrawer(false) }} className="bg-neutral-700 p-3 rounded-full float-end">
+      <X className="h-6 w-6 text-muted/70" /></button>
+
+    <h1 className={`text-4xl font-medium py-4 text-center mt-10 ${instrumentSerif.className}`}>Get in Touch </h1>
+
+      <div className="flex flex-col md:w-3/5 mx-auto mt-10 gap-20">
+        <div className="md:w-3/5 mx-auto flex flex-col">
+          <input className="p-2 px-3 bg-neutral-800 rounded-md border border-neutral-500" type="text" placeholder="Enter Name" name="name"/>
+          <input className="mt-2 p-2 px-3 bg-zinc-50 rounded-md border border-zinc-200" type="email" placeholder="Enter Email" name="email" />
+
+          <p className="text-zinc-400 my-2 text-sm text-center">We&apos;ll Get back to you with-in 24 hours.</p>
+        </div>
+
+        <button className="bg-zinc-700 w-3/5 mx-auto p-3 shadow-inner shadow-zinc-400  text-zinc-200 font-semibold rounded-lg">Book Free call</button>
+      </div>
+  </motion.div>
+}
